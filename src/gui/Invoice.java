892d7ef382b42bafd79f2ab17e4d51095ffeba08
Hirusha21 <geekhirusha@gmail.com>
2024-09-07 11:57:19 +0530

    set stock update code

--- src/gui/Invoice.java
+++ src/gui/Invoice.java
@@ -789,28 +789,35 @@ public class Invoice extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         try {
-            
-            String invoiID = jTextField1.getText();
+
+            String invoiceID = jTextField1.getText();
             String customerMobile = jTextField2.getText();
             String employeeEmail = jLabel3.getText();
             String dateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
             String paidAmount = paymentField.getText();
             String paymentMethodID = paymentHashMap.get(String.valueOf(jComboBox1.getSelectedItem()));
             String discount = String.valueOf(discountField.getText());
-            
+
             //Insert to Invoice
-            MySQL2.executeIUD("INSERT INTO `invoice` VALUES ('"+invoiID+"', '"+customerMobile+"', '"+dateTime+"', '"+employeeEmail+"', '"+paidAmount+"', '"+paymentMethodID+"', '"+discount+"')");
-            
-            // insert Into Invoice Item
-            
-            // Stock Update
-            
-            
-            
+            MySQL2.executeIUD("INSERT INTO `invoice` VALUES ('" + invoiceID + "', '" + customerMobile + "', '" + dateTime + "', '" + employeeEmail + "', '" + paidAmount + "', '" + paymentMethodID + "', '" + discount + "')");
+
+            for (InvoiceItem invoiceItem : invoiceItemMap.values()) {
+
+                // insert Into Invoice Item  
+                MySQL2.executeIUD("INSERT INTO `invoice_item` (`stock_id`, `qty`, `invoice_id`)"
+                        + " VALUES ('" + invoiceItem.getStockID() + "', '" + invoiceItem.getQty() + "', '" + invoiceID + "')");
+                // insert Into Invoice Item
+
+                // Stock Update
+                MySQL2.executeIUD("UPDATE `stock` SET `qty`=`qty`-'" + invoiceItem.getQty() + "' WHERE `id` = '" + invoiceItem.getStockID() + "'");
+                // Stock Update
+            }
+
+            // withdraw Points
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
